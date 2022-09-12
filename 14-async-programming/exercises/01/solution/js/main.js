1139b04b7dcd1c8319f5b6d63179d21e5e40bd45
Mónika <gergye.monika@gmail.com>
2022-09-12 19:05:02 +0200

    javított verzió

--- 14-async-programming/exercises/01/solution/js/main.js
+++ 14-async-programming/exercises/01/solution/js/main.js
@@ -1,7 +1,8 @@
 import ajaxRequest from './ajaxRequest.js';
 import { actions } from './store/users.js';
 
-const urls = ['./json1/users1.json', './json/users2.json', './json/users3.json'];
+//const urls = ['./json1/users1.json', './json/users2.json', './json/users3.json'];
+const urls = ['/exercises/01/solution/json/users1.json', '/exercises/01/solution/json/users2.json', '/exercises/01/solution/json/users3.json'];
 
 function successCallback(responseText) {
   actions.setUsers(responseText);
